bca2ad5d5d67ef5fe12a6529286e1368f0aaa437
unknown <Anu-wolf>
2025-07-11 11:27:13 +0530

    align 4

--- Hop_lvl/script_hop.js
+++ Hop_lvl/script_hop.js
@@ -427,12 +427,12 @@ function startStoneThrow(round, callback) {
     let fracY, x;
     if (step === 1) { fracY = 7/8; x = droppableRect.left + droppableRect.width/2 - xOffset + shiftX; }
     else if (step === 2) { fracY = 7/8; x = droppableRect.left + droppableRect.width/2 + xOffset + shiftX; }
-    else if (step === 3) { fracY = 6/8; x = droppableRect.left + droppableRect.width/2 + shiftX; }
+    else if (step === 3) { fracY = 7/8; x = droppableRect.left + droppableRect.width/2 + shiftX; }
     else if (step === 4) { fracY = 5/8; x = droppableRect.left + droppableRect.width/2 - xOffset + shiftX; }
     else if (step === 5) { fracY = 5/8; x = droppableRect.left + droppableRect.width/2 + xOffset + shiftX; }
-    else if (step === 6) { fracY = 4/8; x = droppableRect.left + droppableRect.width/2 + shiftX; }
-    else if (step === 7) { fracY = 3/8; x = droppableRect.left + droppableRect.width/2 - xOffset + shiftX; }
-    else if (step === 8) { fracY = 3/8; x = droppableRect.left + droppableRect.width/2 + xOffset + shiftX; }
+    else if (step === 6) { fracY = 7/8; x = droppableRect.left + droppableRect.width/2 + shiftX; }
+    else if (step === 7) { fracY = 5/8; x = droppableRect.left + droppableRect.width/2 - xOffset + shiftX; }
+    else if (step === 8) { fracY = 5/8; x = droppableRect.left + droppableRect.width/2 + xOffset + shiftX; }
     else { fracY = 7/8; x = droppableRect.left + droppableRect.width/2 + shiftX; }
     const y = droppableRect.top + droppableRect.height * fracY;
     return { x, y };
